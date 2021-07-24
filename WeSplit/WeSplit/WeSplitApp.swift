fa4620b261967385392d702fe9b9c597a8ae3d1a
Steven Yu <stevenyuser@gmail.com>
2021-07-24 19:29:25 -0400

    First commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Steven Yu on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
