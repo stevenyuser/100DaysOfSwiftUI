6d93ccc69d1ebccd973f53bbb4f0a60ead9bd210
Steven Yu <stevenyuser@gmail.com>
2021-07-29 23:42:02 -0400

    day 27 done

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Steven Yu on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
